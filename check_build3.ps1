fe25f64530f32a1a47df9c82d504bfb4c7051112
siva.subramanian <sivasubramanian86@hotmail.com>
2026-07-05 22:14:09 +0530

    chore: remove debug ps1 scripts; update Firebase site to pragmalogixai-app.web.app

--- check_build3.ps1
+++
@@ -1,6 +0,0 @@
-$token = (gcloud auth print-access-token 2>&1)
-$uri = "https://cloudbuild.googleapis.com/v1/projects/genai-apac-2026-491004/builds/52e4eb62-8aeb-4b2f-a39b-7f9c0366c985"
-$headers = @{ Authorization = "Bearer $token" }
-$resp = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get
-Write-Host "Status: $($resp.status)"
-Write-Host "Finish: $($resp.finishTime)"
